861e21abcc1c6c67b6451791610aeba2e480fb5b
Giuseppe Grieco <g.grieco1997@gmail.com>
2026-05-22 00:34:38 +0200

    upgrade git config

--- home/giuseppeg/programs/git.nix
+++ home/giuseppeg/programs/git.nix
@@ -4,23 +4,25 @@
   programs.git = {
     enable = true;
 
-    userName = "Giuseppe Grieco";
-    userEmail = "g.grieco1997@gmail.com";
+    settings = {
+      user = {
+        name = "Giuseppe Grieco";
+        email = "g.grieco1997@gmail.com";
+      };
 
-    extraConfig = {
       init.defaultBranch = "main";
       pull.rebase = true;
       fetch.prune = true;
       core.editor = "nvim";
       push.autoSetupRemote = true;
-    };
 
-    aliases = {
-      st = "status";
-      co = "checkout";
-      br = "branch";
-      cm = "commit -m";
-      lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %s %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
+      alias = {
+        st = "status";
+        co = "checkout";
+        br = "branch";
+        cm = "commit -m";
+        lg = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %s %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all";
+      };
     };
   };
 }

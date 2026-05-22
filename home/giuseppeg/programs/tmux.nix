1c2966a3d4126c2c6e30c1f7278f30187aadd5dc
Giuseppe Grieco <g.grieco1997@gmail.com>
2026-05-22 20:54:22 +0200

    add tmux config

--- home/giuseppeg/programs/tmux.nix
+++ home/giuseppeg/programs/tmux.nix
@@ -9,9 +9,36 @@
     baseIndex = 1;
     escapeTime = 0;
 
+    keyMode = "vi";
+
+    historyLimit = 50000;
+
+    sensibleOnTop = false;
+
     plugins = with pkgs.tmuxPlugins; [
+      resurrect
+      {
+        plugin = continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-save-interval '15'
+        '';
+      }
     ];
 
-    extraConfig = "";
+    extraConfig = ''
+      # Vim-style pane navigation
+      bind h select-pane -L
+      bind j select-pane -D
+      bind k select-pane -U
+      bind l select-pane -R
+
+      # Renumber windows when one is closed
+      set -g renumber-windows on
+
+      # Copy to the Wayland system clipboard in vi copy-mode
+      bind -T copy-mode-vi v send-keys -X begin-selection
+      bind -T copy-mode-vi y send-keys -X copy-pipe-and-cancel "wl-copy"
+    '';
   };
 }

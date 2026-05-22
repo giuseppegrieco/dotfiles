88da81b21549c7eac2363a8a2b3b9d68e462317e
Giuseppe Grieco <g.grieco1997@gmail.com>
2026-05-22 09:39:25 +0200

    fixup! fix waybar workspaces style

--- home/giuseppeg/programs/waybar.nix
+++ home/giuseppeg/programs/waybar.nix
@@ -136,11 +136,14 @@
       }
 
       /* workspaces as circles (no labels) */
-      #workspaces { padding: 5px; }
+      #workspaces { padding: 0 10px; }
       #workspaces button {
+        padding: 0;
+        /* vertical margin shrinks the button to match its width -> true circle */
+        margin: 6px 4px;
         min-width: 14px;
         min-height: 14px;
-        border-radius: 100%;
+        border-radius: 50%;
         color: transparent;
         background: ${base03};
       }

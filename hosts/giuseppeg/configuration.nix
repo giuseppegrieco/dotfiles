8ecca5543970af1849624bcbb261e54b8ea62f87
Giuseppe Grieco <g.grieco1997@gmail.com>
2026-05-23 23:44:43 +0200

    fixup! finalize laptop config

--- hosts/giuseppeg/configuration.nix
+++ hosts/giuseppeg/configuration.nix
@@ -1,18 +1,17 @@
 { config, pkgs, ... }:
 
 {
-	imports = [
-		../shared/configuration.nix
-		./hardware-configuration.nix
-	];
+  imports = [
+    ../shared/configuration.nix
+    ./hardware-configuration.nix
+  ];
 
-	swapDevices = [
-		{
-			device = "/var/lib/swapfile"
-			size = 4096;
-		}
-	];
+  swapDevices = [
+    {
+      device = "/var/lib/swapfile";
+      size = 4096;
+    }
+  ];
 
-	networking.hostName = "giuseppeg";
+  networking.hostName = "giuseppeg";
 }
-

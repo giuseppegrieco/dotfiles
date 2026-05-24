a0c0290b9dd597f6336503dd99b6b719d698ac2e
Giuseppe Grieco <g.grieco1997@gmail.com>
2026-05-24 22:14:36 +0200

    fixup! idle external power fix

--- home/giuseppeg/programs/hyprland.nix
+++ home/giuseppeg/programs/hyprland.nix
@@ -132,8 +132,8 @@ in
       ];
 
       bindl = [
-        ", switch:on:Lid Switch, exec, hyprctl keyword monitor \"eDP-1,disable\""
-        ", switch:off:Lid Switch, exec, hyprctl keyword monitor \"eDP-1,preferred,auto,1.25\""
+        ", switch:on:Lid Switch, exec, hyprctl --batch \"keyword monitor HDMI-A-1,preferred,auto,1 ; keyword monitor eDP-1,disable\""
+        ", switch:off:Lid Switch, exec, hyprctl --batch \"keyword monitor eDP-1,preferred,auto,1.25 ; keyword monitor HDMI-A-1,preferred,auto,1,mirror,eDP-1\""
       ];
     };
   };

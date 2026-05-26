47ae6b202ef4b408e8de62f17265817ce42cb648
Giuseppe Grieco <g.grieco1997@gmail.com>
2026-05-26 15:52:05 +0200

    fix monitor rules

--- home/giuseppeg/programs/hyprland.nix
+++ home/giuseppeg/programs/hyprland.nix
@@ -132,7 +132,7 @@ in
       ];
 
       bindl = [
-        ", switch:on:Lid Switch, exec, hyprctl --batch \"keyword monitor HDMI-A-1,preferred,auto,1 ; keyword monitor eDP-1,disable\""
+        ", switch:on:Lid Switch, exec, sh -c 'grep -q ^connected /sys/class/drm/card*-HDMI-A-1/status && hyprctl --batch \"keyword monitor HDMI-A-1,preferred,auto,1 ; keyword monitor eDP-1,disable\"'"
         ", switch:off:Lid Switch, exec, hyprctl --batch \"keyword monitor eDP-1,preferred,auto,1.25 ; keyword monitor HDMI-A-1,preferred,auto,1,mirror,eDP-1\""
       ];
     };

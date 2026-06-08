08c758d2ad228806bc12277b359ad4ab9b8e2c6b
Giuseppe Grieco <g.grieco1997@gmail.com>
2026-06-08 21:42:23 +0200

    add default clangd config

--- home/giuseppeg/programs/nvim.nix
+++ home/giuseppeg/programs/nvim.nix
@@ -17,4 +17,15 @@
   };
 
   xdg.configFile."nvim/lua".source = ./nvim;
+
+  # Global clangd config so loose .cpp files (no compile_commands.json) resolve
+  # the stdlib. On NixOS clangd can't find GCC's headers on its own; telling it
+  # to use g++ as the driver makes it query the wrapper for libstdc++ includes
+  # (e.g. <bits/stdc++.h>). Paired with --query-driver in plugins/lsp.lua.
+  xdg.configFile."clangd/config.yaml".text = ''
+    CompileFlags:
+      Compiler: ${pkgs.gcc}/bin/g++
+      Add:
+        - -std=c++23
+  '';
 }
